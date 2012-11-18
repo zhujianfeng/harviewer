938f59134d274e7ef48de69e6216c348c6c023fe
zhujianfeng <zhujianfeng2004@126.com>
2012-11-18 16:39:11 +0800

    Revert "首次上传"
    This reverts commit 08cf8480f13e751e123acf91c67197524e7608a3.

--- selenium/tests/config.php
+++
@@ -1,5 +0,0 @@
-<?php 
-# don't forget the trailing slash
-$harviewer_base = "http://legoas/har/viewer/webapp-build/";
-$test_base = "http://legoas/har/viewer/selenium/";
-?>
--- wordpress/plugins/harviewer/readme.txt
+++
@@ -1,4 +0,0 @@
-=== Plugin Name ===
-Author: Jan Odvarko
-Home: http://www.janodvarko.cz
-Tags: har, viewer
